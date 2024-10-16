ab21e2de9bb53be58304579c67c1540fe4b54370
Bhavishya Sahay <sahaybhavishya@gmail.com>
2024-10-16 15:33:21 +0530

    commit

--- src/services/voter.js
+++ src/services/voter.js
@@ -10,7 +10,9 @@ class VoterService {
   }
   async createVoter(voterDetails = this.details) {
     const voter = await prisma.voter.create({
-      //write your code here
+      email: voterDetails.email,
+      name: voterDetails.name,
+      password: bcrypt.hashSync(voterDetails.password, 4),
     });
     return { voter, message: "Account created Successfully!" };
   }
